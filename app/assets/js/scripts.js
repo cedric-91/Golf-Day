7f85336e13fa319d05cd90f6ccc29bba9ababc8c
Cedric Tientcheu <cedric___100@hotmail.co.uk>
2017-07-11 15:50:19 +0100

    Add files via upload

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * Golf_Day
+ * Fiercely quick and opinionated front-ends
+ * trinitymirror.com
+ * @author Cedric Tientcheu | Trinity Mirror
+ * @version 1.0.0
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
